a2b53dd5bbbd1c35ed80aaae1ee4df30eea52029
Aayushi1130 <32549040+Aayushi1130@users.noreply.github.com>
2021-10-18 19:48:02 +0530

    head_issues

--- polls/apps.py
+++ polls/apps.py
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+
 from django.apps import AppConfig
 
 
 class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
-=======
-from django.apps import AppConfig
 
-
-class PollsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'polls'
->>>>>>> adf5822c86b4b40b740343c06ecd77ff788bcd1c
