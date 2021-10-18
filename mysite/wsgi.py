1bdbbfcac9d0215626eb9e5c12ec4773edc617c3
Aayushi1130 <32549040+Aayushi1130@users.noreply.github.com>
2021-10-18 19:24:28 +0530

    head_issues

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 WSGI config for mysite project.
 
@@ -15,7 +15,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
-=======
 """
 WSGI config for mysite project.
 
@@ -32,4 +31,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
->>>>>>> adf5822c86b4b40b740343c06ecd77ff788bcd1c
